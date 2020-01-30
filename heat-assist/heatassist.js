363b9b0aea31d09ad830bd16d9938e308f058797
jaunius@gmx.com <jaunius@gmx.com>
2020-01-30 02:24:06 +0200

    fixed typo in installation description

--- heat-assist/heatassist.js
+++ heat-assist/heatassist.js
@@ -10,6 +10,7 @@
  *  - install at above path
  *  - add to /store/scripts/ovmsmain.js:
  *        heatasist = require("lib/heatasist");
+ *        heatAssist.startAssist()
  *  - script reload
  * 
  * Config:
